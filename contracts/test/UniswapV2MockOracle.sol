dbd57cfcbd24c346999f79f6b2f6d6e5d032e7a5
Your Name <you@example.com>
2022-08-30 10:24:28 +0200

    using on chain twap for trait locking

---
+++ contracts/test/UniswapV2MockOracle.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.4;
+
+import "../interfaces/IUniswapV2Oracle.sol";
+
+contract UniswapV2MockOracle is IUniswapV2Oracle {
+    uint256 price;
+
+    constructor(uint256 _price) {
+        price = _price;
+    }
+
+    function consultAndUpdateIfNecessary(address, uint256)
+        external
+        override
+        returns (uint256)
+    {
+        return price;
+    }
+
+    function setPrice(uint256 _price) external {
+        price = _price;
+    }
+}
