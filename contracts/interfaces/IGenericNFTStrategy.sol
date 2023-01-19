ddbe87788248747ffff2845458167a644a3ddc09
Your Name <you@example.com>
2023-01-19 22:17:07 +0100

    Added sewer pass claiming

--- contracts/interfaces/IGenericNFTStrategy.sol
+++ contracts/interfaces/IGenericNFTStrategy.sol
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.4;
 
-interface INFTStrategy {
-
+interface IGenericNFTStrategy {
     enum Kind {
         STANDARD,
         FLASH
@@ -10,6 +9,4 @@ interface INFTStrategy {
 
     function kind() external view returns (Kind);
     function depositAddress(address _account) external view returns (address);
-    function afterDeposit(address _owner, uint256[] calldata _nftIndexes, bytes calldata _data) external;
-    function withdraw(address _owner, address _recipient, uint256 _nftIndex) external;
 }
--- contracts/test/MockStandardStrategy.sol
+++ contracts/test/MockStandardStrategy.sol
@@ -2,9 +2,9 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import "../interfaces/INFTStrategy.sol";
+import "../interfaces/IStandardNFTStrategy.sol";
 
-contract MockStandardStrategy is INFTStrategy {
+contract MockStandardStrategy is IStandardNFTStrategy {
 
     IERC721 nft;
     bool sendBack;
@@ -25,12 +25,22 @@ contract MockStandardStrategy is INFTStrategy {
             nft.transferFrom(address(this), _recipient, _nftIndex);
     }
 
+    function flashLoanStart(address, address _recipient, uint256[] calldata _nftIndexes, bytes calldata) external override returns (address) {
+        for (uint256 i; i < _nftIndexes.length; i++) {
+            nft.transferFrom(address(this), _recipient, _nftIndexes[i]);
+        }
+
+        return address(this);
+    }
+
+    function flashLoanEnd(address, uint256[] calldata, bytes calldata) external override {}
+
     function depositAddress(address) external view override returns (address) {
         return address(this);
     }
 
-    function kind() external pure override returns (INFTStrategy.Kind) {
-        return INFTStrategy.Kind.STANDARD;
+    function kind() external pure override returns (Kind) {
+        return Kind.STANDARD;
     }
 
 }
