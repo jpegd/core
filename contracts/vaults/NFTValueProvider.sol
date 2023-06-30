bd612b1083ea73f0663ab5459ac70786071eddbd
spaghettieth <79546291+spaghettieth@users.noreply.github.com>
2023-06-30 11:53:07 +0200

    replaced AccessControlUpgradeable with custom version to make storage compatible with OwnableUpgradeable

--- contracts/vaults/NFTValueProvider.sol
+++ contracts/vaults/NFTValueProvider.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
 
+import "../utils/AccessControlUpgradeable.sol";
 import "../utils/RateLib.sol";
 
 import "../interfaces/IAggregatorV3Interface.sol";
