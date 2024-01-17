103155f8d4c317f38364703eedfca2ecd551f45f
0xlucian <0xluciandev@gmail.com>
2024-01-17 15:44:54 +0200

    revert code change in token contract

--- contracts/tokens/JPEG.sol
+++ contracts/tokens/JPEG.sol
@@ -10,8 +10,8 @@ contract JPEG is ERC20Votes, AccessControl {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
     constructor(uint256 totalSupply)
-        ERC20("JPEG'd", "JPGD")
-        ERC20Permit("JPEG'd")
+        ERC20("JPEG", "JPEG")
+        ERC20Permit("JPEG")
     {
         _mint(msg.sender, totalSupply);
         _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
